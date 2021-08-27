0d1260e98daa02f08d57c118ad4e5caeec8db879
Olivia-Debora Moisi <deboramoisi@yahoo.com>
2021-08-27 09:17:59 +0300

    Some updates, better organization of files

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,9 @@
+import React from "react";
 import ExpenseDate from './ExpenseDate';
-import Card from './Card';
+import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
 
     return (
         <Card className="expense-item">
@@ -12,6 +13,7 @@ function ExpenseItem(props) {
                 <div className="expense-item__price">
                     ${props.amount}
                 </div>
+                <button>Change Title</button>
             </div>
         </Card>
     );
